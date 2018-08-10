522b9aa288314b5952dcf041c6d1862df59583df
Gabryel Souza <Raduziel@users.noreply.github.com>
2018-08-10 20:43:18 -0300

    Add files via upload

--- DeitiesOfFaerun/Lib/Spheres/Shaman/__SHMKN.d
+++ DeitiesOfFaerun/Lib/Spheres/Shaman/__SHMKN.d
@@ -2,5 +2,5 @@
 	BEGIN ~%MOD_PREFIX%SHMKN~
 	IF	~True()~	THEN	BEGIN	0
 		SAY	(AT tra_start)
-		IF	~True()~	THEN	REPLY	(AT tra_quit)	DO	~~	EXIT
+		IF	~False()~	THEN	REPLY	(AT tra_quit)	DO	~~	EXIT
 	END
